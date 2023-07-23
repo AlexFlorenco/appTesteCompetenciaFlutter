c6623992aac6526a6ec2fce103639d63352bb5d1
AlexFlorenco <aflorenco13@gmail.com>
2023-07-23 20:35:12 -0300

    style: Criação de tokens de estilo

---
+++ lib/colors/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const Color bgPrimaryColor = Color(0xFF1E2226);
+const Color bgSecondaryColor = Color(0xFF373C40);
+
+const Color appSecondaryColor = Color(0xFF6FB5F8);
+const Color appPrimaryColor = Color(0xFFFFFFFF);
+
+const Color textPrimaryColor = Color(0xFFFFFFFF);
+const Color textPrimaryColorDark = Color(0xFF000000);
+const Color textSecondaryColor = Color(0xFFABB1B5);
+
+const Color buttonBgPrimaryEnabled = Color(0xFF6FB5F8);
+const Color buttonBgPrimaryDisabled = Color(0xFF393C40);
+const Color buttonFgPrimaryEnabled = Color(0xFF1D3C5C);
+const Color buttonFgPrimaryDisabled = Color(0xFF75797E);
